a1f311f691268dfbb99cbddcca73421de1aec578
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-03-07 14:53:46 -0300

    agregando propiedades js, en css editando indicators para que tenga forma de diamante

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll('.carousel');
     M.Carousel.init(elementosCarousel, {
-        duration:150
+        duration:150,
+        dist:-80,/* calculo la prespectiva */
+        shift:5,
+        padding:5,
+        numVisible: 3, /* establescon cuadno carousel van a ser visibles */
+        indicators: true,/* los puntitos que indican la imagen */
+        noWrap:false/* inicia con el primer slide y termina en el ultimo con true, es decir hay a la izquiera y derecha */
+        
     });
 });
